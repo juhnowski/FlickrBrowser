c8109718834e577eb1d7baa5358d4189480d94d7
Juhnowski <juhnowski@gmail.com>
2018-11-11 03:45:30 +0300

    first commit

---
+++ app/src/main/java/com/juhnowski/ilya/flikrbrowser/GetRawData.kt
@@ -0,0 +1,66 @@
+package com.juhnowski.ilya.flikrbrowser
+
+import android.os.AsyncTask
+import android.util.Log
+import java.io.IOException
+import java.lang.Exception
+import java.net.MalformedURLException
+import java.net.URL
+
+enum class DownloadStatus {
+    OK, IDLE, NOT_INITIALIZED, FAILED_OR_EMPTY, PERMISSION_ERROR, ERROR
+}
+
+class GetRawData(private val listener: OnDownloadComplete) : AsyncTask<String, Void, String>() {
+    private val TAG = "GetRawData"
+    private var downloadStatus = DownloadStatus.IDLE
+
+    interface OnDownloadComplete {
+        fun onDownloadComplete(data: String, status: DownloadStatus)
+    }
+
+//    private var listener: MainActivity? = null
+//
+//    fun setDownloadCompleteListener(callbackObject: MainActivity) {
+//        listener = callbackObject
+//    }
+
+    override fun onPostExecute(result: String) {
+        Log.d(TAG, "onPostExecute called")
+        listener?.onDownloadComplete(result,downloadStatus)
+    }
+
+    override fun doInBackground(vararg params: String?): String {
+        if (params[0] == null) {
+            downloadStatus = DownloadStatus.NOT_INITIALIZED
+            return "No URL specified"
+        }
+
+        try {
+            downloadStatus = DownloadStatus.OK
+            return URL(params[0]).readText()
+        } catch (e: Exception) {
+            val errorMessage = when (e) {
+                is MalformedURLException -> {
+                    downloadStatus = DownloadStatus.NOT_INITIALIZED
+                    "doInBackground: Invalid URL ${e.message}"
+                }
+                is IOException -> {
+                    downloadStatus = DownloadStatus.FAILED_OR_EMPTY
+                    "doInBackground: IO Exception reading Data ${e.message}"
+                }
+                is SecurityException -> {
+                    downloadStatus = DownloadStatus.PERMISSION_ERROR
+                    "doInBackground: SecurityException: Needs permission? ${e.message}"
+                }
+                else -> {
+                    downloadStatus = DownloadStatus.ERROR
+                    "Unknown error: ${e.message}"
+                }
+            }
+
+            Log.d(TAG, errorMessage)
+            return errorMessage
+        }
+    }
+}
---
+++ app/src/main/java/com/juhnowski/ilya/flikrbrowser/SearchActivity.kt
@@ -0,0 +1,19 @@
+package com.juhnowski.ilya.flikrbrowser
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_search.*
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
